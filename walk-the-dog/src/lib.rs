9671a265953190c41211fd5ab2050c8454eb420e
Keita Nonaka <iKonnyaku40@gmail.com>
2023-09-10 12:43:54 -0700

    feat: display image

--- walk-the-dog/src/lib.rs
+++ walk-the-dog/src/lib.rs
@@ -33,12 +33,24 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    sierpinski(
-        &context,
-        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-        &Color(0, 255, 0),
-        7,
-    );
+    wasm_bindgen_futures::spawn_local(async move {
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
+        let image = web_sys::HtmlImageElement::new().unwrap();
+        let callback = Closure::once(move || {
+            let _ = success_tx.send(());
+        });
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_src("Idle (1).png");
+        let _ = success_rx.await;
+        let _ = context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+
+        sierpinski(
+            &context,
+            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
+            &Color(0, 255, 0),
+            7,
+        );
+    });
     Ok(())
 }
 
