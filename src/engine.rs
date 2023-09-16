07abd382f681cdeee8bef6dae294ad30bf35725e
Keita Nonaka <iKonnyaku40@gmail.com>
2023-09-16 02:03:51 -0700

    feat: implement keyboard input control

--- src/engine.rs
+++ src/engine.rs
@@ -196,3 +196,9 @@ fn process_input(state: &mut KeyState, keyevent_receiver: &mut UnboundedReceiver
         };
     }
 }
+
+#[derive(Clone, Copy)]
+pub struct Point {
+    pub x: i16,
+    pub y: i16,
+}
