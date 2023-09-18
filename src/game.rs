6eca215785b2de1610fcd5d956dd7620284e12e1
Keita Nonaka <iKonnyaku40@gmail.com>
2023-09-17 22:17:29 -0700

    refactor: switch to the new rhb with game state, and remove the old code

--- src/game.rs
+++ src/game.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use crate::{
     browser,
-    engine::{self, Game, KeyState, Point, Rect, Renderer},
+    engine::{self, Game, KeyState, Rect, Renderer},
 };
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
@@ -24,28 +24,18 @@ struct Cell {
     frame: SheetRect,
 }
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize)]
 pub struct Sheet {
     frames: HashMap<String, Cell>,
 }
 
 pub struct WalkTheDog {
-    image: Option<HtmlImageElement>,
-    sheet: Option<Sheet>,
-    frame: u8,
-    position: Point,
     rhb: Option<RedHatBoy>,
 }
 
 impl WalkTheDog {
     pub fn new() -> Self {
-        WalkTheDog {
-            image: None,
-            sheet: None,
-            frame: 0,
-            position: Point { x: 0, y: 0 },
-            rhb: None,
-        }
+        WalkTheDog { rhb: None }
     }
 }
 
@@ -58,80 +48,41 @@ impl Game for WalkTheDog {
         let image = Some(engine::load_image("rhb.png").await?);
 
         Ok(Box::new(WalkTheDog {
-            image: image.clone(),
-            sheet: sheet.clone(),
-            frame: self.frame,
-            position: self.position,
             rhb: Some(RedHatBoy::new(
-                sheet.clone().ok_or_else(|| anyhow!("No Sheet Present"))?,
-                image.clone().ok_or_else(|| anyhow!("No Image Present"))?,
+                sheet.ok_or_else(|| anyhow!("No Sheet Present"))?,
+                image.ok_or_else(|| anyhow!("No Image Present"))?,
             )),
         }))
     }
 
     fn update(&mut self, keystate: &KeyState) {
-        let mut velocity = Point { x: 0, y: 0 };
         if keystate.is_pressed("ArrowDown") {
-            velocity.y += 3;
+            unimplemented!()
         }
 
         if keystate.is_pressed("ArrowUp") {
-            velocity.y -= 3;
+            unimplemented!()
         }
 
         if keystate.is_pressed("ArrowRight") {
-            velocity.x += 3;
             self.rhb.as_mut().unwrap().run_right();
         }
 
         if keystate.is_pressed("ArrowLeft") {
-            velocity.x -= 3;
-        }
-
-        self.position.x += velocity.x;
-        self.position.y += velocity.y;
-
-        if self.frame < 23 {
-            self.frame += 1;
-        } else {
-            self.frame = 0;
+            unimplemented!()
         }
         self.rhb.as_mut().unwrap().update();
     }
 
     fn draw(&self, renderer: &Renderer) {
-        let current_sprite = (self.frame / 3) + 1;
-        let frame_name = format!("Run ({}).png", current_sprite);
-        let sprite = self
-            .sheet
-            .as_ref()
-            .and_then(|sheet| sheet.frames.get(&frame_name))
-            .expect("Cell not found");
-
         renderer.clear(&Rect {
             x: 0.0,
             y: 0.0,
             width: 600.0,
             height: 600.0,
         });
-        if let Some(image) = self.image.as_ref() {
-            renderer.draw_image(
-                image,
-                &Rect {
-                    x: sprite.frame.x.into(),
-                    y: sprite.frame.y.into(),
-                    width: sprite.frame.w.into(),
-                    height: sprite.frame.h.into(),
-                },
-                &Rect {
-                    x: self.position.x.into(),
-                    y: self.position.y.into(),
-                    width: sprite.frame.w.into(),
-                    height: sprite.frame.h.into(),
-                },
-            );
-            self.rhb.as_ref().unwrap().draw(renderer);
-        }
+
+        self.rhb.as_ref().unwrap().draw(renderer);
     }
 }
 
